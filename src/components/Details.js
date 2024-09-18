6dd6b37220ddf8e710e9c9fbfac3e175148c4825
Adrian <adricardo679@gmail.com>
2024-09-17 22:23:04 -0700

    Details.js fix

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,35 +1,38 @@
 import React from "react";
 import Card from "./Card.js";
 
-const Details = ({details}) => {
+const Details = ({ details }) => {
     const detailsList = {
-        name: "Name" ,
-        country: "Country" ,
-        currency: "Currency" ,
-        exchange: "Exchange" ,
+        name: "Name",
+        country: "Country",
+        currency: "Currency",
+        exchange: "Exchange",
         ipo: "IPO Date",
-        marketCapitalization: "Market Capitalization" , 
-        finnhubIndustry: "Industry" ,
+        marketCapitalization: "Market Capitalization",
+        finnhubIndustry: "Industry",
     };
 
     const convertMillionToBillion = (number) => {
-        return(number/1000).toFixed(2);
+        return (number / 1000).toFixed(2);
     };
 
     return (
         <Card>
-        <ul className="w-full h-full flex-col justify-between divide-y-1">
-            {Object.keys(detailsList).map((item) => {
-                return (
-                <li key={item}  className="flex-1 justify-between p-2">
-                    <span>{detailsList[item]}</span>
-                    <span>{item === "marketCapitalization"? '${convertMillionToBillion(details[item])}B':
-                     details[item]}</span>
-                </li>
-                );
-            })}
-        </ul>
-    </Card>
+            <ul className="w-full h-full flex-col divide-y-1">
+                {Object.keys(detailsList).map((item) => {
+                    return (
+                        <li key={item} className="flex justify-between p-2">
+                            <span>{detailsList[item]}</span>
+                            <span>
+                                {item === "marketCapitalization"
+                                    ? `$${convertMillionToBillion(details[item])}B`
+                                    : details[item]}
+                            </span>
+                        </li>
+                    );
+                })}
+            </ul>
+        </Card>
     );
 };
 
