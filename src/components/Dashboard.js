8341afe36f0f6c7601db3bb7ce0ac8e0eb40081d
Adrian <adricardo679@gmail.com>
2024-09-17 22:10:26 -0700

    28:03

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,11 @@ import React from "react";
 import Card from "./Card.js";
 import { mockCompanyDetails } from "../constants/mock.js";
 import Header from "./Header.js";
+import Details from "./Details.js";
 
 const Dashboard = () => {
     return (
-    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand ${
-        darkMode ? ">
+    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand ${ darkMode ? ">
         <div className=" col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
         <Header name={mockCompanyDetails.name}/>
         </div>
@@ -16,8 +16,8 @@ const Dashboard = () => {
         <div>
             <Card>Overview</Card> 
         </div>
-        <div className="">
-            <Card>Details</Card> 
+        <div className="row-span-2 xl:row-span-3">
+            <Details details={mockCompanyDetails} />
         </div>
     </div>
     );
