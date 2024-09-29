b9c2e4d0e860024b1ac15ce327129906fef4b180
Adrian <adricardo679@gmail.com>
2024-09-29 16:22:13 -0700

    21:00 part 7

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,41 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { mockCompanyDetails } from "../constants/mock.js";
 import Header from "./Header.js";
 import Details from "./Details.js";
 import Overview from "./Overview.js";
 import Chart from "./Charts.js";
 import ThemeContext from "../context/ThemeContext.js";
+import StockContext from "../context/StockContext.js";
+import { fetchQuote, fetchStockDetails } from "../api/stock-api.js";
 
 const Dashboard = () => {
     const {darkMode} = useContext(ThemeContext);
+    const {stockSymbol} = useContext(StockContext);
+
+    const [stockDetails,setStockDetails] = useState({});
+    const [quote,setQuote] = useState({});
+
+    useEffect(() => {
+        const updateStockDetails = async() => {
+            try{
+                const result = await fetchStockDetails(stockSymbol);
+                setStockDetails(result);
+            } catch(error){
+                setStockDetails({});
+                console.log(error);
+            }
+        };
+
+        const updateStockOverview = async () => {
+            try {
+                const result = await fetchQuote(stockSymbol);
+                setQuote(result);
+            } catch(error) {
+                setQuote({});
+                console.log(error);
+            }
+        };
+    }, [stockSymbol]);
     return (
     <div
      className={`h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand ${
