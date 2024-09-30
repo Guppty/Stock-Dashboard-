9f62d22b29021d12a6e4cfee56f0074e722b6e30
Adrian <adricardo679@gmail.com>
2024-09-29 17:31:24 -0700

    debugging

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -84,7 +84,7 @@ useEffect(() => {
                     </li>
                 ))}
             </ul>
-            <ResponsiveContainer  width="100%" height={300} >
+            <ResponsiveContainer  >
                 <AreaChart data={data}>
                     <defs>
                         <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
